38b296e2ebc404a79a5ee42052f76d85ef97c6a1
Sergio <Sergio@sergio-pc>
2014-08-16 16:00:17 +0200

    Dodanie pakietu utils ze starej aplikacji

---
+++ CMS/src/main/java/pl/edu/pjwstk/cms/utils/ConnectionManager.java
@@ -0,0 +1,146 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.pjwstk.cms.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * Klasa zarządzająca połączeniem z bazą danych; Aby wykonywać połączenia z 
+ * bazą danych należy korzystać z zwawrtych w niej metod.
+ * @author sergi_000
+ */
+public class ConnectionManager {
+
+    public static String staticUrl = "jdbc:mysql://78.46.82.172/paffel_cms?useUnicode=true&characterEncoding=UTF-8&collation=utf8_polish_ci";
+    public static String staticLogin = "paffel";
+    public static String staticPass = "leofram";
+    
+    private final static Logger LOGGER = Logger.getLogger("utils.ConnectionManager");
+    private String url = "jdbc:mysql://cms.cchl9cz5cp1p.us-west-2.rds.amazonaws.com:3306/cms?user=admin&password=cmsadmin&useUnicode=true&characterEncoding=UTF-8";
+    private String login = "admin";
+    private String pass = "cmsadmin";
+    private Connection connection;
+    private static Map<String, ConnectionManager> activeConnections = new HashMap<>();
+
+    
+    public void setDBUrl(String url) {
+        this.url = url;
+    }
+    
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    public void setPassword(String password) {
+        pass = password;
+    }
+    
+    protected ConnectionManager() {
+        
+    }
+    
+    public static ConnectionManager getConnectionManager(String login, String password, String url) {
+        if(activeConnections.containsKey(login)){
+            return activeConnections.get(login);
+        } else {
+            ConnectionManager cm = new ConnectionManager();
+            cm.setLogin(login);
+            cm.setDBUrl(url);
+            cm.setPassword(password);
+            activeConnections.put(login, cm);
+            return activeConnections.get(login);
+        }
+        
+    }
+    
+    public void newConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Connection c = null;
+
+        try {
+            c = DriverManager.getConnection(
+                    url, login, pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connection = c;
+    }
+
+    public boolean closeConnection() {
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                newConnection();
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+        return connection;
+
+
+    }
+
+    /**
+     * Send a select type query to the database; Returns a set of records wich
+     * match the requirements of the query.
+     * @param query
+     * @return 
+     */
+    public ResultSet select(String query) {
+        ResultSet rs = null;
+        //System.out.println("DEBUG: Execute query - "+query);
+        LOGGER.info("Execute query - "+query);
+        try {
+            Statement s = getConnection().createStatement();
+            rs = s.executeQuery(query);            
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+
+        return rs;
+    }
+    /**
+     * Send an update type query to the databse; Returns true or false depending
+     * if the insertion or update was succesful.
+     * @param query
+     * @return 
+     */
+    public boolean update(String query) {
+        //System.out.println("DEBUG: Execute query - "+query);
+        LOGGER.info("Execute query - "+query);
+        try {
+            Statement s = getConnection().createStatement();
+            s.executeUpdate(query);            
+            return true;
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            return false;
+        }
+    }
+}
---
+++ CMS/src/main/java/pl/edu/pjwstk/cms/utils/Utils.java
@@ -0,0 +1,165 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.pjwstk.cms.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Tutaj przetrzymywane będą różne przydatne metody wykorzystywane w więcej niż
+ * jedenj klasie.
+ *
+ * @author Sergio
+ */
+public abstract class Utils {
+
+    /**
+     * Metoda zamieniająca JSON w postaci Stringa na object Javovy; zwracany
+     * obiekt wciąż trzeba zrzutować do konkretnego obiektu.
+     * @param json
+     * @param javaClass
+     * @return 
+     */
+    public static Object convertJSONStringToObject(String json, Class javaClass) {       
+        ObjectMapper mapper = new ObjectMapper();
+        Object o = null;
+        try {
+            o = mapper.readValue(json, javaClass);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return o;
+    }
+    /**
+     * Metoda zamieniająca JSON w postaci Stringa (z nazwą obiektu na początku)
+     * na object Javovy; zwracany
+     * obiekt wciąż trzeba zrzutować do konkretnego obiektu.
+     *
+     * @param json - JSON w formie Stringa
+     * @param objectName - Nazwa zmiennej String w której zawarty jest JSON
+     * (innymi słowy nazwa zmiennej ktora jest podana jako pierwszy paramter)
+     * @param javaClass - Klasa na podstawie której ma zostać stworzony zwracany
+     * obiekt
+     * @return Obiekt java typu <Object>
+     */
+    public static Object convertJSONStringToObject(String json, String objectName, Class javaClass) {
+        String actualJsonString = json.substring(
+                ("'" + objectName + "':'").length(), json.length() - 1);
+        ObjectMapper mapper = new ObjectMapper();
+        Object o = null;
+        try {
+            o = mapper.readValue(actualJsonString, javaClass);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return o;
+    }
+
+    public static String convertObjectToJSON(Object obj) {
+        System.out.println("DEBUG: requestJsons");
+        ObjectMapper mapper = new ObjectMapper();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] data;
+        try {
+            mapper.writeValue(out, obj);
+            data = out.toByteArray();
+            return new String(data, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String convertObjectListToJSON(List<?> list) {
+        System.out.println("DEBUG: requestJsons");
+        ObjectMapper mapper = new ObjectMapper();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] data;
+        try {
+            mapper.writeValue(out, list);
+            data = out.toByteArray();
+            return new String(data, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String convertOMapToJSON(Map<?, ?> map) {
+        System.out.println("DEBUG: requestJsons");
+        ObjectMapper mapper = new ObjectMapper();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] data;
+        try {
+            mapper.writeValue(out, map);
+            data = out.toByteArray();
+            return new String(data, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Metoda przygotowuje odpowiedź serwera pod kątem pobrania pliku
+     * @param hash
+     * @param fileName
+     * @param mimeType
+     * @param response
+     */
+    public static void download(String hash, String fileName, String mimeType, HttpServletResponse response) {
+        File file;
+        try {
+            byte[] barr = HexConverter.toBytesFromHex(hash);
+            file = new File(fileName);
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(barr);
+            fos.close();
+
+            FileInputStream fis = new FileInputStream(file);
+            response.setContentType(mimeType);
+
+            String headerKey = "Content-Disposition";
+            String headerValue = String.format("attachment; filename=\"%s\"",
+                    fileName);
+            response.setHeader(headerKey, headerValue);
+
+            IOUtils.copy(fis, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException io) {
+            System.err.println("Download IO error");
+            io.printStackTrace();
+        }
+    }
+    
+    /**
+     * Metoda służąca do odpowiedniego przygotowania JSONa z danymi aby prawidłowo przekazać je do klienta (np. do angulara).
+     * @param session <HttpSession> 
+     * @param initData Dane, które mają zostać zamienione w JSON
+     * @return dane odpowiedzi serwera
+     */
+    /*
+    public static ResponseEntity<String> createResponseEntity(HttpSession session, Map<String, Object> initData){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        SystemConfigurationDao sysConfigDao = new SystemConfigurationDao();
+        SystemConfiguration charset = sysConfigDao.findByField("name","DefaultPageEncoding").get(0);
+        responseHeaders.add("Content-Type", "text/html; charset="+charset.getValue());
+        UserDTO user = (UserDTO)(session.getAttribute("user"));
+        initData.put("privileges", user.getPrivilegeKeyCodes());
+        
+        Logger.getLogger(Utils.class.getName()).log(Level.INFO, "SEND: {0}", Utils.convertOMapToJSON(initData));
+        
+        return new ResponseEntity<>(convertOMapToJSON(initData), responseHeaders, HttpStatus.OK);
+    }
+    */
+}

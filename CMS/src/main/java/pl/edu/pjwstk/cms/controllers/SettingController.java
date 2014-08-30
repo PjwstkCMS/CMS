3c32d2810220b306c411f844733163454ce0bb19
Konrad <Konrad@Konrad-Komputer>
2014-08-30 14:46:45 +0200

    dodane controlllery

---
+++ CMS/src/main/java/pl/edu/pjwstk/cms/controllers/SettingController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pl.edu.pjwstk.cms.controllers;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import pl.edu.pjwstk.cms.controllers.general.BaseController;
+import pl.edu.pjwstk.cms.dao.general.GenericDao;
+import pl.edu.pjwstk.cms.models.Customer;
+import pl.edu.pjwstk.cms.utils.ConnectionManager;
+/**
+ *
+ * @author Konrad
+ */
+public class SettingController extends BaseController {
+
+    private final static Logger LOGGER = Logger.getLogger(SettingController.class.getName());
+
+    public SettingController() {
+
+    }
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("newjsp");
+        model.addObject("msg", "HelloGuestController");
+        
+        return model;
+    }
+}
+

e25fc542771d0e3c63e8295f784df3e6bbe275ef
sergio <sergio@sergio-xubuntu>
2014-10-03 17:54:20 +0200

    Modyfikacje programu CzytnikaKart

--- CzytnikKart/src/Card.java
+++ CzytnikKart/src/Card.java
@@ -1,29 +1,41 @@
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
 /**
  *
- * @author pawelek
+ * @author sergio
  */
-public class Card {
+public class Card extends DatabaseObject{
     
-    long num;
+    private static final Logger LOGGER = Logger.getLogger(Card.class.getName());
+    
+    private String employeeId;
     boolean working = false;
     List<Date[]> dates = new ArrayList<>();
     
     static Map<String, Card> persistance = new HashMap<>();
     
     public Card() {
-        
+        super();
     }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+    
+    
 }
